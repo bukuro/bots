d3454df3587c66217c6c5fd5e1e9058362928d33
bukuro <e.soraluze@gmail.com>
2014-02-17 09:19:14 +0100

    Initial commit

---
+++ bestNBAPlays.py
@@ -0,0 +1,39 @@
+﻿import re
+import time
+import urllib2
+import json
+import twitter
+
+api=twitter.Api(consumer_key='',
+		consumer_secret='',
+		access_token_key='',
+		access_token_secret='')
+
+regex_top_string = '(Top (10|5) NBA (Assists|Dunks|Plays|Blocks))|((Assists|Dunks|Plays|Blocks) of the Week)'
+regex_top = re.compile(regex_top_string, re.IGNORECASE)
+
+last_entry = None 
+
+while True:
+        response = urllib2.urlopen('http://gdata.youtube.com/feeds/api/users/nba/uploads?v=2&alt=json&q=%22top%22&order‌​by=published&max-results=3').read().decode('utf-8')
+        data = json.loads(response)
+        data_feed_entry = data['feed']['entry']
+        first_link = None
+
+        for entries in data_feed_entry:
+                if (not first_entry):
+                        first_entry = entries['id']['$t']
+                if (last_entry == entries['id']['$t']):
+                        break
+                result = regex_top.search(entries['title']['$t'])
+                if (result):
+                        video_title = entries['title']['$t']
+                        video_link = 'youtu.be/' + entries['media$group']['yt$videoid']['$t']
+                        try:
+                                status = api.PostUpdate(video_title + ' ' + video_link + ' #NBA')
+                        except Exception as e:
+                                print('Error', e)
+                                exit(1)
+                        
+        last_entry = first_entry
+        time.sleep(1800)
---
+++ firstHalfBot.py
@@ -0,0 +1,83 @@
+import feedparser
+import re
+import praw
+import time
+
+
+
+def submit_comment(video_link):
+        try:
+                submission.add_comment('[First half highlights](' + video_link + ')')
+        except praw.errors.RateLimitExceeded as e:
+                #sleep until the rate limit let the bot comment again
+                time.sleep(e.sleep_time)
+                submit_comment(video_link)
+	
+	
+try:
+        r = praw.Reddit('NBA first half highlights bot')
+        r.login('', '')
+except Exception as e:
+        print("Error", e)
+        exit(1)
+
+teams_dictionary = {
+				"celtics":"Boston Celtics",
+				"nets":"Brooklyn Nets",
+				"knicks":"New York Knicks",
+				"sixers":"Philadelphia 76ers",
+				"raptors":"Toronto Raptors",
+				"bulls":"Chicago Bulls",
+				"cavaliers":"Cleveland Cavaliers",
+				"pistons":"Detroit Pistons",
+				"pacers":"Indiana Pacers",
+				"bucks":"Milwaukee Bucks",
+				"hawks":"Atlanta Hawks",
+				"bobcats":"Charlotte Bobcats",
+				"heat":"Miami Heat",
+				"magic":"Orlando Magic",
+				"wizards":"Washington Wizards",
+				"mavericks":"Dallas Mavericks",
+				"rockets":"Houston Rockets",
+				"grizzlies":"Memphis Grizzlies",
+				"pelicans":"New Orleans Pelicans",
+				"spurs":"San Antonio Spurs",
+				"nuggets":"Denver Nuggets",
+				"timberwolves":"Minnesota Timberwolves",
+				"thunder":"Oklahoma City Thunder",
+				"blazers":"Portland Trail Blazers",
+				"jazz":"Utah Jazz",
+				"warriors":"Golden State Warriors",
+                                "clippers":"Los Angeles Clippers",
+				"lakers":"Los Angeles Lakers",
+				"suns":"Phoenix Suns",
+				"kings":"Sacramento Kings"}
+	
+
+lastEntry = None
+nba_mod = None
+index_list = 0
+links = []
+while True:
+        nbaFeed = feedparser.parse('http://www.nba.com/topvideo/rss.xml', modified=nba_mod)
+        if (nbaFeed.status is 200):
+                nba_mod = nbaFeed.modified
+                firstEntry = None
+                for entry in nbaFeed.entries:
+                        if (not firstEntry):
+                                firstEntry = entry.id
+                        if (lastEntry == entry.id):
+                                break
+                        elif(entry['title'].find(': First half') is not -1 and not entry.id in links):
+                                links.append(entry.id)
+                                if (len(links) > 15):
+                                        del links[:5]
+                                team_playing = entry['category'][6:]
+                                video_link = entry['link']
+                                submissions = r.search('title:\"Game Thread ' + teams_dictionary[team_playing] + '\"', 'nba', 'new', None, 'day')
+                                for submission in submissions:
+                                        submit_comment(video_link)
+                                        break
+                lastEntry = firstEntry
+        time.sleep(60)
+        
